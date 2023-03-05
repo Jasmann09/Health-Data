e412879dc4a3f594296fd9da3df80c1af91940d5
Jasmann09 <js0289@srmist.edu.in>
2023-03-05 12:23:03 +0530

    Health-Data : React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
+import React from 'react';
+
+// eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const data = {
+    heartRate:120,
+    bloodPressure:120/80,
+    bodyTemp:98.5,
+    respirationRate:97
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div class="container">
+        <div class="card">
+          <h2>Heart Rate</h2>
+          <p id="heart-rate"></p>   
+          <p>{data.heartRate}</p>     
+        </div>
+
+        <div class="card">
+          <h2>Blood Pressure</h2>
+          <p id="blood-pressure"></p>
+          <p>{data.bloodPressure}</p>   
+        </div>
+
+        <div class="card">
+          <h2>Body Temperature</h2>
+          <p id="body-temp"></p>
+          <p>{data.bodyTemp}</p>   
+        </div>
+
+        <div class="card">
+          <h2>Respiration Rate</h2>
+          <p id="respiration-rate"></p>
+          <p>{data.respirationRate}</p>
+        </div>
+        
+      </div>
+
     </div>
   );
 }
